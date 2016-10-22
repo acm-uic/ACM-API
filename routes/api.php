28713dc2d6706f29a0ef3cb967434b099e9dc15e
Edmond Zalenski <ezalenski@payoff.com>
2016-10-22 17:21:09 -0500

    moved api logic to a controller and added google client library to composer dependencies

--- routes/api.php
+++ routes/api.php
@@ -13,53 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/events', function (Request $request) {
-    $client = new Google_Client();
-    $client->useApplicationDefaultCredentials();
-    $client->addScope(Google_Service_Calendar::CALENDAR);
-    $service = new Google_Service_Calendar($client);
-    $current = new DateTime("2016-10-26T17:35:00-05:00");
-    $nextweek = new DateTime("2016-10-26T17:35:00-05:00");
-    $nextweek->add(new DateInterval('P7D'));
-    $optParams = array('singleEvents' => 'true',
-                       'timeMin' => $current->format(DATE_RFC3339),
-                       'timeMax' => $nextweek->format(DATE_RFC3339));
-    $events = $service->events->listEvents('kc72g1ctfg8b88df34qqb62d1s@group.calendar.google.com', $optParams);
-
-    $response = [];
-    $response['Events'] = [];
-    while(true) {
-        foreach ($events->getItems() as $event) {
-            if($event->getStart() != null && $event->getEnd() != null) {
-                if ($event->getStart()->getDateTime() == "") { //all day event
-                    $event_start = new DateTime($event->getStart()->getDate());
-                    $event_end = new DateTime($event->getEnd()->getDate());
-                } else {
-                    $event_start = new DateTime($event->getStart()->getDateTime());
-                    $event_end = new DateTime($event->getEnd()->getDateTime());
-                }
-                if ($event_start <= $current && $current <= $event_end) {
-                    $event_response = [];
-                    $event_response['Name'] = $event->getSummary();
-                    array_push($response['Events'], $event_response);
-                }
-            }
-        }
-
-        $pageToken = $events->getNextPageToken();
-        if ($pageToken) {
-            $optParams = array('singleEvents' => 'true',
-                               'pageToken' => $pageToken,
-                               'timeMin' => $current->format(DATE_RFC3339),
-                               'timeMax' => $nextweek->format(DATE_RFC3339));
-            $events = $service->events->listEvents('kc72g1ctfg8b88df34qqb62d1s@group.calendar.google.com', $optParams);
-        } else {
-            break;
-        }
-    }
-
-    return json_encode($response);
-});
+Route::get('/events', 'EventsController@retrieveEvents');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
