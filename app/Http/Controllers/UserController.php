5197b9519c8b48ab9d16d5f1d887b7989c9aaf38
Edmond Zalenski <ezalenski@payoff.com>
2016-10-29 18:16:00 -0500

    broke large functions into smaller ones and cleaned up logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,36 +11,31 @@ use App\User;
 class UserController extends Controller
 {
     public function createUser(Request $request) {
-        $response = [];
         $userUIN = $request->input("uin");
         $userUserName = $request->input("username");
         $userEmail = $request->input("email");
-
         try {
             $user = UserController::findOrCreateUser((int)$userUIN, $userUserName, $userEmail);
         }
         catch(ErrorException $e) {
-            return response(["error" => $e->getTraceAsString()], 500);
-        }
-        if(!$user) {
-            $response["error"] = "UIN not found";
-            return response($response, 404);
-        } else if(!$user->username) {
-            $response["error"] = "unable to find ACM account with information given";
-            return response($response, 300);
+            return response(["error" => "Internal Server Error"], 500);
         }
 
-        $response["firstName"] = $user->first;
-        $response["lastName"] = $user->last;
-        $response["netid"] = $user->netid;
+        $errorResponse = UserController::verifyUser($user);
+        if($errorResponse) {
+            return $errorResponse;
+        }
         $points = TransactionController::getPointsTotal($user->uin);
-        $response["points"] = $points;
+        $response = [
+            "firstName" => $user->first,
+            "lastName"  => $user->last,
+            "netid"     => $user->netid,
+            "points"    => $points
+        ];
         return response($response, 200);
     }
 
     public function retrieveUser(Request $request, $uin) {
-        $response = [];
-
         try {
             $user = UserController::findOrCreateUser((int)$uin);
         }
@@ -48,107 +43,113 @@ class UserController extends Controller
             return response(["error" => "Internal Server Error"], 500);
         }
 
-        if($user) {
-            if($user->username) {
-                $response["firstName"] = $user->first;
-                $response["lastName"] = $user->last;
-                $response["netid"] = $user->netid;
-                $points = TransactionController::getPointsTotal($user->uin);
-                $response["points"] = $points;
-                return response($response, 200);
-            } else {
-                $response["error"] = "unable to find ACM account with only UIN";
-                return response($response, 300);
-            }
+        $errorResponse = UserController::verifyUser($user);
+        if($errorResponse) {
+            return $errorResponse;
+        }
+        $points = TransactionController::getPointsTotal($user->uin);
+        $response = [
+            "firstName" => $user->first,
+            "lastName"  => $user->last,
+            "netid"     => $user->netid,
+            "points"    => $points
+        ];
+        return response($response, 200);
+    }
+
+    public static function verifyActiveUser($user)
+    {
+        if(!$user) {
+            return response(["error" => "UIN not found"], 404);
+        } else if(!$user->username) {
+            return response(["error" => "unable to find ACM account with only UIN"], 300);
+        } else if(!$user->active) {
+            return response(["error" => "user is an inactive ACM member"], 403);
         } else {
-            $response["error"] = "UIN not found";
-            return response($response, 404);
+            return null;
+        }
+    }
+
+    public static function verifyUser($user) {
+        if(!$user) {
+            return response(["error" => "UIN not found"], 404);
+        } else if(!$user->username) {
+            return response(["error" => "unable to find ACM account with only UIN"], 300);
+        } else {
+            return null;
         }
     }
 
     public static function findOrCreateUser($uin, $username = null, $email = null)
     {
         $user = User::where('uin', $uin)->get()->first();
-        if($user) {
-            return $user;
-        } else {
-            //break uic and acm queries into separate functions
-            $ldapUICUser = getenv('LDAP_UIC_USERNAME');
-            $ldapUICPass = getenv('LDAP_UIC_PASSWORD');
-            $ldapUICServer = getenv('LDAP_UIC_HOST');
-            $ldapUICBase = getenv('LDAP_UIC_GROUP');
-            $ldapACMUser = getenv('LDAP_ACM_USERNAME');
-            $ldapACMPass = getenv('LDAP_ACM_PASSWORD');
-            $ldapACMServer = getenv('LDAP_ACM_HOST');
-            $ldapACMBase = getenv('LDAP_ACM_GROUP');
-            $ldapUICConn = @ldap_connect($ldapUICServer);
-            $ldapACMConn = @ldap_connect($ldapACMServer);
-            if ($ldapUICConn && $ldapACMConn) {
-                $ldapUICBind = @ldap_bind($ldapUICConn, $ldapUICUser, $ldapUICPass);
-                $ldapACMBind = @ldap_bind($ldapACMConn, $ldapACMUser, $ldapACMPass);
-                if ($ldapUICBind) {
-                    $result = ldap_search($ldapUICConn, $ldapUICBase, "(employeeid=$uin)");
-                    $data = ldap_get_entries($ldapUICConn, $result);
-
-                    for ($i = 0; $i < $data["count"]; $i++) {
-                        $user = new User;
-
-                        $userFirstName = $data[$i]["givenname"]["0"];
-                        $userLastName = $data[$i]["sn"]["0"];
-                        $userNetID = $data[$i]["cn"]["0"];
-
-                        $user->uin = $uin;
-                        $user->netid = $userNetID;
-                        $user->first = $userFirstName;
-                        $user->last = $userLastName;
-                    }
-                } else {
-                    throw new ErrorException("Unable to connect to UIC ad");
-                }
-
-                if($ldapACMBind && $user) {
-                    $query = "(&(cn=$user->first $user->last)";
-                    if($username) {
-                        $query .= "(samaccountname=$username)";
-                    }
-                    if($email) {
-                        $query .= "(mail=$email)";
-                    }
-                    $query .= ")";
-                    
-                    $result = ldap_search($ldapACMConn, $ldapACMBase, $query);
-                    $data = ldap_get_entries($ldapACMConn, $result);
-
-                    for($i = 0; $i < $data["count"]; $i++) {
-                        $userEmail = $data[$i]["mail"]["0"];
-                        $userUserName = $data[$i]["samaccountname"]["0"];
-                        $userGroupsString = "";
-                        for($j = 0; $j < $data[$i]["memberof"]["count"]; $j++) {
-                            $userGroupString = $data[$i]["memberof"][$j];
-                            $userGroupsString .= $userGroupString;
-                        }
-                        $userGroups = str_getcsv($userGroupsString);
-                        foreach($userGroups as $group) {
-                            if(str_contains($group, "ACMPaid") == true)
-                                $user->active = true;
-                        }
-
-                        $user->email = $userEmail;
-                        $user->username = $userUserName;
-
-                        $user->save();
-                    }
-                } else {
-                    if(!$ldapACMBind) {
-                        throw new ErrorException("Unable to connect to ACM ad");
-                    }
-                }
+        if(!$user) {
+            $user = UserController::getInfoFromUIC($uin);
+            if($user) {
+                $user = UserController::createFromACM($user, $username, $email);
             }
-
-            ldap_close($ldapUICConn);
-            ldap_close($ldapACMConn);
-
-            return $user;
         }
+        return $user;
+    }
+
+
+    private static function getInfoFromUIC($uin)
+    {
+        $user = null;
+
+        $ldapUICUser = getenv('LDAP_UIC_USERNAME');
+        $ldapUICPass = getenv('LDAP_UIC_PASSWORD');
+        $ldapUICServer = getenv('LDAP_UIC_HOST');
+        $ldapUICBase = getenv('LDAP_UIC_GROUP');
+
+        $ldapUICConn = @ldap_connect($ldapUICServer);
+        if($ldapUICConn) {
+            $ldapUICBind = @ldap_bind($ldapUICConn, $ldapUICUser, $ldapUICPass);
+            if ($ldapUICBind) {
+                $result = ldap_search($ldapUICConn, $ldapUICBase, "(employeeid=$uin)");
+                $data = ldap_get_entries($ldapUICConn, $result);
+                $user = new User;
+                $user->updateFromUICData($data);
+            } else {
+                throw new ErrorException("Unable to connect to UIC ad");
+            }
+        }
+        ldap_close($ldapUICConn);
+        return $user;
+    }
+
+    private static function createFromACM($user, $username, $email)
+    {
+        $ldapACMUser = getenv('LDAP_ACM_USERNAME');
+        $ldapACMPass = getenv('LDAP_ACM_PASSWORD');
+        $ldapACMServer = getenv('LDAP_ACM_HOST');
+        $ldapACMBase = getenv('LDAP_ACM_GROUP');
+        $ldapACMConn = @ldap_connect($ldapACMServer);
+        if ($ldapACMConn) {
+            $ldapACMBind = @ldap_bind($ldapACMConn, $ldapACMUser, $ldapACMPass);
+            if($ldapACMBind) {
+                $query = UserController::createQuery($user, $username, $email);
+                $result = ldap_search($ldapACMConn, $ldapACMBase, $query);
+                $data = ldap_get_entries($ldapACMConn, $result);
+                $user->updateFromACMData($data);
+            } else {
+                throw new ErrorException("Unable to connect to ACM ad");
+            }
+        }
+        ldap_close($ldapACMConn);
+        return $user;
+    }
+
+    private static function createQuery($user, $username, $email)
+    {
+        $query = "(&(cn=$user->first $user->last)";
+        if($username) {
+            $query .= "(samaccountname=$username)";
+        }
+        if($email) {
+            $query .= "(mail=$email)";
+        }
+        $query .= ")";
+        return $query;
     }
 }
--- app/User.php
+++ app/User.php
@@ -19,4 +19,39 @@ class User extends Model
     ];
 
     protected $table = 'users';
+
+    public function updateFromACMData($data)
+    {
+        if($data["count"]) {
+            $userEmail = $data[0]["mail"][0];
+            $userUserName = $data[0]["samaccountname"][0];
+
+            $userGroupsString = "";
+            for($j = 0; $j < $data[0]["memberof"]["count"]; $j++) {
+                $userGroupString = $data[0]["memberof"][$j];
+                $userGroupsString .= $userGroupString;
+            }
+
+            $userGroups = str_getcsv($userGroupsString);
+            foreach($userGroups as $group) {
+                if(str_contains($group, "ACMPaid") == true)
+                    $this->active = true;
+            }
+
+            $this->email = $userEmail;
+            $this->username = $userUserName;
+
+            $this->save();
+        }
+    }
+
+    public function updateFromUICData($data)
+    {
+        if($data["count"]) {
+            $this->uin = $data[0]["employeeid"][0];
+            $this->netid = $data[0]["cn"][0];
+            $this->first = $data[0]["givenname"][0];
+            $this->last = $data[0]["sn"][0];
+        }
+    }
 }
